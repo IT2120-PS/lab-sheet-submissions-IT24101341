f9342219e9bbd640497101dd8ab4252f95355028
IT24101341 <it24101341@my.sliit.lk>
2025-09-16 12:08:15 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24101341/IT24101341_Lab_07.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\a\\Desktop\\IT24101341")
+# Q1
+a <- 0
+b <- 40
+p1 <- (25 - 10) / (b - a)
+p1
+# Q2
+lambda <- 1/3
+p2 <- pexp(2, rate=lambda)
+p2
+# Q3 (i)
+p3_i <- 1 - pnorm(130, mean=100, sd=15)
+p3_i
+# Q3 (ii)
+p3_ii <- qnorm(0.95, mean=100, sd=15)
+p3_ii
+
